f60cb5394e9e9c1b5de50fea3177d961f0d4e1b8
Debasmita Majumder <97617824+debasmita-13@users.noreply.github.com>
2025-02-04 04:21:05 +0530

    Time: 1 ms (94.12%) | Memory: 42.6 MB (71.90%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -5,26 +5,20 @@ class Solution {
             return 0;
 
         int incr=1, decr=1, res=1;
-        for(int i=1; i<nums.length; i++){
-            if(nums[i-1]<nums[i])
-            {
-                incr++;
-                res = Math.max(incr,res);
-            }
-            else
-                incr = 1;
-        }
-
-        for(int i=1; i<nums.length; i++){
-            if(nums[i-1]>nums[i])
-            {
+        for(int i=1; i< nums.length; i++){
+            if(nums[i-1]==nums[i]){
+                incr=1;
+                decr=1;
+            }else 
+            if(nums[i-1]>nums[i]){
+                incr=1;
                 decr++;
-                res = Math.max(decr,res);
+            } else{
+                incr++;
+                decr=1;
             }
-            else
-                decr = 1;
+            res = Math.max(res, Math.max(incr,decr));
         }
-
         return res;
     }
 }
